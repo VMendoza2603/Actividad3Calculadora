057e45f61e9e347ab1c286430f881fa0a9e149cb
VMendoza2603 <vmendozam@unemi.edu.ec>
2026-06-17 17:51:17 -0500

    Correcciones Pylint: agregados docstrings al módulo y funciones

--- calculadora.py
+++ calculadora.py
@@ -1,7 +1,13 @@
+"""Módulo calculadora con operaciones matemáticas básicas."""
+
 def suma(a, b):
+    """Retorna la suma de a y b."""
     return a + b
 
+
 def resta(a, b):
+    """Retorna la resta de a menos b."""
     return a - b
+
 
 print(suma(5, 3))
